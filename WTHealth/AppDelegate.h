e90cc1262397763f460712c9e26356bd49d978dd
wangTao <twang1@unknownspace.com>
2015-09-30 12:30:58 +0800

    Initial Commit

---
+++ WTHealth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WTHealth
+//
+//  Created by wadewade on 15/9/30.
+//  Copyright (c) 2015年 WT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WTHealth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTHealth
+//
+//  Created by wadewade on 15/9/30.
+//  Copyright (c) 2015年 WT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
